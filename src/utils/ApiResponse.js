2919631a2eb2c80ffac7d74bd8eeadc1f43ab38d
nitinbhandari05 <bhandarinitin93@gmail.com>
2026-03-25 12:06:34 +0530

    util code

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,9 @@
+class ApiResponse {
+    constructor(statusCode, data,
+        message = "something went worng",){
+             this.statusCode = statusCode
+             this.data=data
+            this.message = message
+            this.success = statusCode <400
+        }
+}
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,31 @@
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise
+            .resolve(requestHandler(req, res, next))
+            .catch((err) => next(err));
+    };
+};
+
+
+
+export{asyncHandler};
+
+
+
+
+
+
+
+
+// const asyncHandler = (fn) => async (req,res,next) => {
+//     try{
+
+//     }catch(err){
+
+//         res.status(err.code).json({
+//             succses:false,
+//             message: err.message
+//         })
+
+//     }
+// }
